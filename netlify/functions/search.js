ee5ddf5ef2569bc38ce14938515e8bd54ed8578f
noxrsmusic-source <noxrsmusic@gmail.com>
2026-09-05 14:01:18 +0530

    Update search.js

--- netlify/functions/search.js
+++ netlify/functions/search.js
@@ -1,9 +1,25 @@
-// Built-in HTTPS module (No npm install required, 100% crash-proof on Netlify)
 const https = require('https');
 
 exports.handler = async function(event) {
-  const query = (event.queryStringParameters && event.queryStringParameters.query) 
-    ? event.queryStringParameters.query 
+  // CORS Headers
+  const headers = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers": "Content-Type, Authorization, X-Requested-With",
+    "Access-Control-Allow-Methods": "GET, OPTIONS",
+    "Content-Type": "application/json"
+  };
+
+  // Browser preflight OPTIONS request handle karna
+  if (event.httpMethod === "OPTIONS") {
+    return {
+      statusCode: 204,
+      headers: headers,
+      body: ""
+    };
+  }
+
+  const query = (event.queryStringParameters && event.queryStringParameters.query)
+    ? event.queryStringParameters.query
     : 'Trending';
 
   const saavnUrl = `https://www.jiosaavn.com/api.php?__call=search.getResults&_marker=0&q=${encodeURIComponent(query)}&p=1&n=25&_format=json&ctx=web6dot0`;
@@ -24,22 +40,14 @@ exports.handler = async function(event) {
       res.on('end', () => {
         resolve({
           statusCode: 200,
-          headers: {
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Headers": "Content-Type",
-            "Access-Control-Allow-Methods": "GET",
-            "Content-Type": "application/json"
-          },
+          headers: headers,
           body: data
         });
       });
     }).on('error', (err) => {
       resolve({
         statusCode: 500,
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-          "Content-Type": "application/json"
-        },
+        headers: headers,
         body: JSON.stringify({ error: err.message })
       });
     });
