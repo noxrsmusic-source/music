d256870f9adc3dd27a5b627b207ef9cc7ec8823a
noxrsmusic-source <noxrsmusic@gmail.com>
2026-09-06 12:35:13 +0530

    Update search.js

--- api/search.js
+++ api/search.js
@@ -1,1 +1,38 @@
+const https = require('https');
 
+exports.handler = async function(event) {
+  const headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'Content-Type',
+    'Access-Control-Allow-Methods': 'GET, OPTIONS',
+    'Content-Type': 'application/json; charset=utf-8'
+  };
+
+  if (event.httpMethod === 'OPTIONS') {
+    return { statusCode: 200, headers, body: '' };
+  }
+
+  const query = (event.queryStringParameters && event.queryStringParameters.query)
+    ? event.queryStringParameters.query
+    : 'Bollywood Trending';
+
+  // Normal Search ya Artist Recommendation fetch karega
+  const saavnUrl = `https://www.jiosaavn.com/api.php?__call=search.getResults&_marker=0&q=${encodeURIComponent(query)}&p=1&n=15&_format=json&ctx=web6dot0`;
+
+  return new Promise((resolve) => {
+    https.get(saavnUrl, {
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36',
+        'Referer': 'https://www.jiosaavn.com/'
+      }
+    }, (res) => {
+      let data = '';
+      res.on('data', chunk => data += chunk);
+      res.on('end', () => {
+        resolve({ statusCode: 200, headers, body: data });
+      });
+    }).on('error', (err) => {
+      resolve({ statusCode: 500, headers, body: JSON.stringify({ error: err.message }) });
+    });
+  });
+};
